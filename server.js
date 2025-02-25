14c18d90f0a1564c8a92441b700054c2f3c7ee16
ddinhthuan <105702028+ddinhthuan@users.noreply.github.com>
2025-02-25 15:37:53 +0700

    Update server.js

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.post("/upload", async (req, res) => {
           content: [
             {
               type: "text",
-              text: "Tôi là một người mù và cần biết có những gì trong bức ảnh tôi vừa chụp. Vui lòng tuân thủ các quy tắc sau, không cần giải thích thêm ngoài nội dung tôi yêu cầu: (1)Nếu ảnh có cảnh vật hoặc phong cảnh, hãy mô tả khung cảnh đó trong khoảng 15 từ, không thừa ý.(2)Nếu ảnh có người hoặc nhiều vật thể, hãy mô tả hoặc liệt kê những người/vật thể chính, mỗi người/vật thể không quá 10 từ, không thừa ý.(3)Nếu ảnh có chữ viết (dù là chữ in hay viết tay), hãy sao chép đầy đủ và chính xác phần chữ đó, không giới hạn số từ và không sửa lỗi chính tả.(4) Nếu ảnh là ảnh chụp người/chân dung, thì tôi cần một vài đặc điểm về khuôn mặt và giới tính, ước lượng độ tuổi(khoảng 20 từ).(5)Không trả lời gì khác ngoài những điều tôi yêu cầu ở trên.(6)Toàn bộ câu trả lời bằng tiếng Việt.(7)chỉ sử dụng kí tự viết thường, viết hoa và dấu câu chấm phẩy, không xuống dòng"
+              text: "Cho tôi biết tên của một vật thể nổi bật nhất trong hình, chỉ cung cấp tên,không giải thích gì thêm"
             },
             {
               type: "image_url",
